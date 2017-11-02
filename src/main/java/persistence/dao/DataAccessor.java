67ea28003450326d8c5cfec076cc8a852b3beb9f
majiasheng <jiasheng.ma@yahoo.com>
2017-11-02 16:29:40 -0400

    upload dao and dataservice implementation

---
+++ src/main/java/persistence/dao/DataAccessor.java
@@ -0,0 +1,21 @@
+package persistence.dao;
+
+import java.util.Collection;
+import java.util.Map;
+
+import model.Coordinate;
+import model.District;
+import model.ElectionData;
+import model.GeoData;
+import model.State;
+
+public interface DataAccessor {
+	public Collection<State> getStates();
+	public State getState(int stateId);
+	public Map<Integer, Collection<Coordinate>> getStateBoundaries();
+	public Collection<District> getDataByYear(int selectedState, int selectedYear);
+	public Collection<GeoData> getGeoDataByYear(int stateId, int year);
+	public Collection<ElectionData> getElectionDataByYear(int stateId, int year);
+	public String getDistrictInfo(int districtId);
+	public boolean upload(Object uploadTarget);
+}
---
+++ src/main/java/persistence/dao/DataAccessorImpl.java
@@ -0,0 +1,54 @@
+package persistence.dao;
+
+import java.util.Collection;
+import java.util.Map;
+
+import model.Coordinate;
+import model.District;
+import model.ElectionData;
+import model.GeoData;
+import model.State;
+
+public class DataAccessorImpl implements DataAccessor {
+
+	public Collection<State> getStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public State getState(int stateId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<Integer, Collection<Coordinate>> getStateBoundaries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<District> getDataByYear(int selectedState, int selectedYear) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<GeoData> getGeoDataByYear(int stateId, int year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<ElectionData> getElectionDataByYear(int stateId, int year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDistrictInfo(int districtId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean upload(Object uploadTarget) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
