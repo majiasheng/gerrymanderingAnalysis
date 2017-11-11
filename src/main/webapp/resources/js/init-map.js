8085659e9350cf5323ecea82f472f77328ccfe26
kkui <kaho.kui@stonybrook.edu>
2017-11-11 18:13:55 -0500

    revised inti-map.js

--- src/main/webapp/resources/js/init-map.js
+++ src/main/webapp/resources/js/init-map.js
@@ -1,6 +1,10 @@
 // init map
 var token = 'pk.eyJ1IjoibWEzMDgiLCJhIjoiY2o4ZGxoa3hyMHJrdDMwbzA5emM5Y3pzcSJ9.ZsR3x4DhKRrkTD7goSnE3w'
-var map1 = L.map('mapid').setView([36.4051421,-95.5136459], 3.91);
+var map1 = L.map('mapid', {
+  center:        L.latLng(36.4051421, -95.5136459),
+  zoom:          3.91,
+  worldCopyJump: true
+});
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
