ea8985bcfaed97ce48ffb41653267f3b8194832f
kkui <kaho.kui@stonybrook.edu>
2017-12-08 16:19:18 -0500

    added show winning percentage

--- src/main/webapp/resources/js/mapManip.js
+++ src/main/webapp/resources/js/mapManip.js
@@ -386,7 +386,12 @@ $(document).ready(function () {
                               var pval = val;
                               val = "";
                               $.each(pval, function (key2, val2){
-                                val += "<br>" + title(key2) + "=" + val2 + ' ';
+                                var pval2 = val2;
+                                val2 = "";
+                                $.each(pval2, function (i3, val3){
+                                  val2 += "<br>" + "Dist"+ (i3+1) + ":" + (val3*100).toLocaleString("en-US") + '% ';
+                                });
+                                val += "<br>" + title(key2) + ":" + val2 + ' ';
                               });
                             } else {
                               return true;
